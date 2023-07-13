bb5ee7103bd2f8f5edd66976d14c8f68d292b52a
Nilesh Patel <40303820+nil2610@users.noreply.github.com>
2023-07-13 09:43:11 +0530

    Add files via upload

--- CypressTest/cypress/e2e/MyTest/test1.cy.js
+++ CypressTest/cypress/e2e/MyTest/test1.cy.js
@@ -2,11 +2,7 @@ describe('Test 1', () => {
 
     it('New T11', () => {
         cy.logger('Start New T11')
-        cy.log(Cypress.env('URL')) 
-        cy.log(Cypress.env('tokenURL'))
-        cy.log(Cypress.env('clientId'))   
-        cy.log(Cypress.env('clientSecret'))
-        cy.log(Cypress.env('apiURL'))
+        cy.log(Cypress.env('URL'))        
         expect(true).to.be.true
     })
 
